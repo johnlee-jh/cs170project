b5649e5e5f6f585ded51264661865bb93a92f31b
John Lee <leejohn24@berkeley.edu>
2021-04-30 13:07:06 -0700

    Step 1 complete

--- solver.py
+++ solver.py
@@ -29,41 +29,66 @@ def solve(G):
         k_val = 100
         c_val = 5
 
-    #Create L, this would be the longest path graph
-    L = G.copy()
+    """ STEP 1: Find Longest Path """
+    #Gives an approximation of the longest path as L.
+    L = nx.Graph()
+    longest_path = longest_simple_paths(L, source=0, target=(V_G-1))
+    L_E = len(longest_path) - 1
+    L_V = len(longest_path)
+    if len(G.edges) - k <= L_E and len(G.nodes) - c <= L_V:
+        #k,c constraints are satisfied. Return G - L
+        pass
+    else:
+        #k,c constraints aren't satisfied. Time for Step 2
+        for i in range(len(longest_path) - 1):
+            u = longest_path[i]
+            v = longest_path[i+1]
+            w_uv = G[u][v]["weight"]
+            #print(u, v, w_uv)
+            L.add_edge(u, v, weight=w_uv)
+    drawGraph(L, "L", True)
 
-    #Create Max Spanning Tree on L
-    L_mst = tree.maximum_spanning_edges(G, algorithm="prim", data=False)
-    L_edges = list(L_mst)
-    print(sorted(sorted(e) for e in L_edges))
-    """
-    for u,v,d in H.edges(data=True):
-        d['weight'] = 1 / d['weight']
-    longest_path_nodes = nx.dijkstra_path(H, 0, len(G.nodes) - 1, weight='weight')
-    size_of_edges = len(longest_path_nodes) - 1
-    size_of_vertices = size_of_edges + 1
-    if len(G.edges) - k <= size_of_edges and len(G.nodes) - c <= size_of_vertices: # i dont think this condition is correct 
-        nodes_delete = []
-        edges_delete = []
-        for node in G.nodes:
-            if node not in longest_path_nodes: # add all nodes that are not in the longest path
-                nodes_delete.append(node)
-        for u,v,d in G.edges():
-            if u in longest_path_nodes:
-                index = longest_path_nodes.index(u)
-                if not (index + 1 < len(longest_path_nodes) - 1 and longest_path_nodes[index + 1] == v):
-                    edges_delete.append((u, v, d)) # edge as a tuple
-        return nodes_delete, edges_delete
-    """
-    drawGraph(L, "L")
-    return None
+    """ STEP 2: Make Longest Path the Only Path """
     
-def drawGraph(G, filename):
-    pos=nx.spring_layout(G) # pos = nx.nx_agraph.graphviz_layout(G)
-    nx.draw_networkx(G,pos)
-    labels = nx.get_edge_attributes(G,'weight')
-    nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
-    plt.savefig("visualizations/" + filename + ".jpg")
+    return None
+
+def drawGraph(G, filename, detail):
+    """
+    Draw the graph G, and store the drawing into the visualizations folder.
+    G -> Graph to visualize
+    filename -> Filepath under visualizations/ to store drawing in
+    detail -> True if you want v & e numbers, False for a plain graph
+    """
+    if (detail == True):
+        pos=nx.spring_layout(G)
+        nx.draw_networkx(G,pos)
+        labels = nx.get_edge_attributes(G,'weight')
+        nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+        plt.savefig("visualizations/" + filename + ".jpg")
+    else: 
+        nx.draw_networkx(G)
+        plt.savefig("visualizations/" + filename + ".jpg")
+
+def longest_simple_paths(graph, source, target):
+    """
+    Randomly sample simple paths from s-t and return the longest
+    path out of the sample.
+    TODO: Make the random path selector more random (not relying on NX)
+    """
+    longest_path = []
+    longest_path_length = 0
+    approx_limit = 5000
+    simple_paths = nx.all_simple_paths(G, source=source, target=target)
+    for path in nx.all_simple_paths(G, source=source, target=target):
+        if approx_limit == 0:
+            break
+        path_length = nx.path_weight(G, path, weight="weight")
+        if path_length > longest_path_length:
+            longest_path_length = path_length
+            longest_path = path
+        approx_limit -= 1
+    print(longest_path_length)
+    return longest_path
 
 # Here's an example of how to run your solver.
 
